c83faefabe8f525fd3eddb5f7180d8ba883ad907
Mathew Jacob <mjacob1002@gmail.com>
2022-07-12 16:40:41 -0400

    Fixed a potential bug in the isAcyclic compartor. Switched algorithm to leverage graph coloring in order to verify if a graph contains a cycle or not.

--- include/utility.h
+++ include/utility.h
@@ -8,7 +8,7 @@ struct isAcyclic{
 	private:
 		bool dfs(Vertex current, const Graph& g);
 		bool dfs(Vertex current, Vertex parent, const Graph& g);
-		std::unordered_set<Vertex> visited;
+		std::unordered_map<Vertex, int> _coloring_of_nodes;
 	public:
 		isAcyclic();
 		bool operator()(const Graph& g);
--- src/utility.cpp
+++ src/utility.cpp
@@ -3,30 +3,34 @@
 isAcyclic::isAcyclic(){}
 
 bool isAcyclic::dfs(Vertex current, const Graph& g){
-	if(visited.count(current)) return false;
-	visited.insert(current);
+	if(_coloring_of_nodes[current] == 1) return false; // "grey" color of the node
+	_coloring_of_nodes[current] = 1;
 	auto adjacency = g._adj.at(current); 
 	for(auto p : adjacency){
-		if(!dfs(p.first, g)) return false;
+		if(_coloring_of_nodes[p.first] != 2 && !dfs(p.first, g)) return false; // if the node isn't completed and the exploration from that node leads to a cycle
 	}
+	_coloring_of_nodes[current] = 2;
 	return true;
 }
 
 bool isAcyclic::dfs(Vertex current, Vertex parent, const Graph& g){
-	if(visited.count(current)) return false;
-	visited.insert(current);
+	if(_coloring_of_nodes[current] == 1) return false; // "grey" color of the node
+	_coloring_of_nodes[current] = 1;
 	auto adjacency = g._adj.at(current);
 	for(auto p : adjacency){
 		if(p.first == parent) continue; // skip the predecessor node in the case of an undirected graph
-		if(!dfs(p.first, current, g)) return false;
+		if(_coloring_of_nodes[p.first] != 2 && !dfs(p.first, current, g)) return false;
 	}
+	_coloring_of_nodes[current] = 2; // completely visited
 	return true;
 }
 
 bool isAcyclic::operator()(const Graph& g){
 	auto adjacency = g._adj;
+	for(auto p : adjacency)
+		_coloring_of_nodes[p.first] = 0;
 	for(auto p : adjacency){
-		if(visited.count(p.first)) continue;
+		if(_coloring_of_nodes[p.first] != 0) continue; // if it isn't 0, then it has already been explored 
 		bool containsCycle;
 		if(g.isDirected()){
 			containsCycle = dfs(p.first, g);
